58ee9926dbc338ae7284745359acceab198df7ff
Indrajit Mandal <im.indrajit16@gmail.com>
2024-06-18 22:55:24 +0530

    Fixed the documentation

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # text-editor-python
-This is the repository to a Text Editor app that I made using Python
+
+
+
+## About
+
+This is a simple text editing application in Python, which supports the basic text operations like Cut, Copy, Paste, Undo, Redo, and also basic file operations like New, Save, and Open. It has some great features like Unlimited Undo and Redo Operations, Dark-mode. 
+
+## Technologies Used
+
+-   Python
+-   Tkinter(Python library for GUI)
+
+## Prerequisite
+
+-   Python3
+-   tkinter library in Python (comes installed in most of the installations)
+
+> If you are facing issues regarding "No module named 'Tkinter'", you can solve the issue from [this](https://stackoverflow.com/questions/25905540/importerror-no-module-named-tkinter) link.
+
+## Demo
+
+ - This is how the text-editor looks like. You can write, edit and delete content in your file.
+
+ ![enter image description here](https://github.com/Minato-namikaze99/text-editor-python/blob/main/screenshots/main_screen.png)
+
+ - This is what it looks like after you edit and save a file.
+
+
+ ![enter image description here](https://github.com/Minato-namikaze99/text-editor-python/blob/main/screenshots/main_screen_updated.png)
--- Window.py
+++ Window.py
@@ -8,11 +8,11 @@ from Stack import *
 class Window:
     def __init__(self):
         # Variables declarations at initial level
-        self.isFileOpen = True
-        self.File = ""
-        self.isFileChange = False
-        self.elecnt = 0
-        self.mode = "normal"
+        self.isFileOpen = True # Variable to check whether the file has been opened or not
+        self.File = "" # Stores teh location of the file
+        self.isFileChange = False # Variable to check whether the file has been changed or not
+        self.elecnt = 0 
+        self.mode = "normal" # Variable to store the view mode of the application
         self.fileTypes = [('All Files', '*.*'),
                           ('Python Files', '*.py'),
                           ('Text Document', '*.txt')]
@@ -28,6 +28,8 @@ class Window:
         # Initialisation of MenuBar
         self.menuBar = Menu(self.window, bg="#eeeeee", font=("Helvetica", 13), borderwidth=0)
         self.window.config(menu=self.menuBar)
+        
+        
         # File Menu
         self.fileMenu = Menu(self.menuBar, tearoff=0, activebackground="#d5d5e2", bg="#eeeeee", bd=2, font="Helvetica")
         self.fileMenu.add_command(label="    New       Ctrl+N", command=self.new_file, )
@@ -36,6 +38,8 @@ class Window:
         self.fileMenu.add_separator()
         self.fileMenu.add_command(label="    Exit          Ctrl+D", command=self._quit)
         self.menuBar.add_cascade(label="   File   ", menu=self.fileMenu)
+        
+        
         # Edit Menu
         self.editMenu = Menu(self.menuBar, tearoff=0, activebackground="#d5d5e2", bg="#eeeeee", bd=2,
                              font="Helvetica", )
@@ -46,24 +50,26 @@ class Window:
         self.editMenu.add_command(label="    Copy    Ctrl+C", command=self.copy)
         self.editMenu.add_command(label="    Paste   Ctrl+V", command=self.paste)
         self.menuBar.add_cascade(label="   Edit   ", menu=self.editMenu)
+        
+        
         # View Menu
         self.viewMenu = Menu(self.menuBar, tearoff=0, activebackground="#d5d5e2", bg="#eeeeee", bd=2,
                              font="Helvetica", )
         self.viewMenu.add_command(label="   Change Mode   ", command=self.change_color)
         self.menuBar.add_cascade(label="   View   ", menu=self.viewMenu)
+
+        
         # Help Menu
         self.helpMenu = Menu(self.menuBar, tearoff=0, activebackground="#d5d5e2", bg="#eeeeee", bd=2,
                              font="Helvetica", )
         self.helpMenu.add_command(label="    About   ", command=self.about)
         self.menuBar.add_cascade(label="   Help   ", menu=self.helpMenu)
 
-        # Initialisation Of Stack Objects By Original state i.e if the file contains data, it is the Original state of
-        # that file
+        # Initialisation Of Stack Objects By Original state i.e if the file contains data, it is the Original state of that file
         self.UStack = Stack(self.TextBox.get("1.0", "end-1c"))
         self.RStack = Stack(self.TextBox.get("1.0", "end-1c"))
 
-    #     Member Functions
-    # 1. New File method which creates a new file
+    # New File method which creates a new file
     def new_file(self):
         self.TextBox.config(state=NORMAL)
         if self.isFileOpen:
@@ -89,7 +95,7 @@ class Window:
             self.UStack.clear_stack()
             self.UStack.add(self.TextBox.get("1.0", "end-1c"))
 
-    # 2. Open a file which opens a file in editing mode
+    # Open a file which opens a file in editing mode
     def open_file(self):
         self.TextBox.config(state=NORMAL)
         if self.isFileOpen and self.isFileChange:
@@ -109,7 +115,7 @@ class Window:
             self.UStack.clear_stack()
             self.UStack.add(self.TextBox.get("1.0", "end-1c"))
 
-    # 3. Save file
+    # Save file
     def save_file(self, file):
         result = message.askquestion('Window Title', 'Do You Want to Save Changes')
         if result == "yes":
@@ -124,7 +130,7 @@ class Window:
         else:
             pass
 
-    # 4. Save new file -> this function is for saving the new file
+    # Save new file -> this function is for saving the new file
     def save_new_file(self, result):
         self.isFileChange = False
         if result == "yes":
@@ -134,13 +140,13 @@ class Window:
         else:
             self.TextBox.delete('1.0', END)
 
-    # 5. Writing in file
+    # Writing in file
     def write_file(self, file):
         inputValue = self.TextBox.get("1.0", "end-1c")
         outfile = open(file, "w")
         outfile.write(inputValue)
 
-    # 6. Getting the data from file and showing in the text widget box
+    # Getting the data from file and showing in the text widget box
     def retrieve_input(self):
         if self.isFileOpen and len(self.File) != 0:
             self.write_file(self.File)
@@ -150,7 +156,7 @@ class Window:
             self.window.wm_title(self.File)
             self.isFileOpen = True
 
-    # 7. This function invokes whenever a key is pressed whether it is a special-key or a normal key
+    # This function invokes whenever a key is pressed whether it is a special-key or a normal key
     def key_pressed(self, event):
         if event.char == "\x1a" and event.keysym == "Z":
             self.redo()
@@ -194,7 +200,7 @@ class Window:
         if self.TextBox.get("1.0", "end-1c") == self.UStack.ele(0):
             self.isFileChange = False
 
-    # 8. Undo the data by calling Stack class functions
+    # Undo the data by calling Stack class functions
     def undo(self):
         self.isFileChange = True
         if self.UStack.size() == 1:
@@ -206,7 +212,7 @@ class Window:
             self.TextBox.delete('1.0', END)
             self.TextBox.insert(END, text)
 
-    # 9. Redo/Rewrite the task/data by calling Stack class functions
+    # Redo/Rewrite the task/data by calling Stack class functions
     def redo(self):
         if self.RStack.size() > 1:
             text = self.RStack.peek()
@@ -215,18 +221,18 @@ class Window:
             self.UStack.add(text)
             self.RStack.remove()
 
-    # 10. Close the window (called when the close button at the right-top is clicked)
+    # Close the window (called when the close button at the right-top is clicked)
     def on_closing(self):
         if self.isFileOpen and self.isFileChange:
             self.save_file(self.File)
         self._quit()
 
-    # 11. Quit or Exit Function to exit from Text-Editor
+    # Quit or Exit Function to exit from Text-Editor
     def _quit(self):
         self.window.quit()
         self.window.destroy()
 
-    # 12. Night mode view by changing the color of Text widget
+    # Dark mode view by changing the color of Text widget
     def change_color(self):
 
         if self.mode == "normal":
@@ -238,26 +244,26 @@ class Window:
             self.TextBox.configure(background="white", foreground="black", font=("Helvetica", 14),
                                    insertbackground="black")
 
-    # 13. About
+    # About
     def about(self):
         outfile = open("About.txt", "r")
         text = outfile.read()
         self.TextBox.insert(END, text)
         self.TextBox.config(state=DISABLED)
 
-    # 14. Copy
+    # Copy
     def copy(self):
         self.TextBox.clipboard_clear()
         text = self.TextBox.get("sel.first", "sel.last")
         self.TextBox.clipboard_append(text)
 
-    # 15. Cut
+    # Cut
     def cut(self):
         self.copy()
         self.TextBox.delete("sel.first", "sel.last")
         self.UStack.add(self.TextBox.get("1.0", "end-1c"))
 
-    # 16. Paste
+    # Paste
     def paste(self):
         text = self.TextBox.selection_get(selection='CLIPBOARD')
         self.TextBox.insert('insert', text)
